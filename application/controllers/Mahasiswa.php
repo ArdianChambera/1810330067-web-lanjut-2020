f2f7915ae563b06c2d7de1f2b687d073075a9ebe
ArdianChambera <ardiancambera@gmail.com>
2020-10-22 16:46:59 +0800

    update controller mahasiswa dan mahasiswa/v_create / upload ke-2

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -6,6 +6,12 @@ class Mahasiswa extends CI_Controller {
 	
 	public function index()
 	{
+		//load dari folder view
 		$this->load->view('mahasiswa/v_index');
+	}
+	
+	public function create()
+	{
+		$this->load->view('mahasiswa/v_create');
 	}
 }

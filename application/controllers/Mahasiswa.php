750752857a9fc5cb2a2d2fa6424ca1544e29b6ed
ArdianChambera <ardiancambera@gmail.com>
2020-11-05 17:24:28 +0800

    menambah link bootstap edit table mahasiswa_index

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -7,11 +7,24 @@ class Mahasiswa extends CI_Controller {
 	public function index()
 	{
 		//load dari folder view
-		$this->load->view('mahasiswa/v_index');
+		$data ['nama_lengkap'] = "Chambera"
+		$data ['jenis_kelamin'] = "Laki"
+		$mahasiswa[0] = array(
+			'nim' => 1810330067
+			'nama' => 'Chambera'
+			'profil' => 'Data Diri'
+		);
+		$data['mahasiswa'] = $mahasiswa;
+		$this->load->view('mahasiswa/v_index',$data);
 	}
 	
 	public function create()
 	{
 		$this->load->view('mahasiswa/v_create');
+	}
+
+	public function tambah()
+	{
+		$this->load->view('add_mhs');
 	}
 }
--- application/views/mahasiswa_index.php
+++ application/views/mahasiswa_index.php
@@ -1,13 +1,49 @@
-<!DOCTYPE html>
+<!DOCTYPE html>	
 <html>
 <head>
 	<title>Mahasiswa Index</title>
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
 </head>
 <body>
-
-	<h1>Mahasiswa</h1>
-	<h3>Selamat Datang</h3>
-	<hr>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a href="" class="navbar-brand">
+			Tugas
+		</a>
+		<div class="navbar-nav">
+			<a href="" class="nav-link active">Home</a>
+			<a href="" class="nav-link">Tambah Data</a>
+		</div>
+		<div class="navbar-nav ml-auto">
+			<a href="" class="nav-link active">cambe</a>
+		</div>
+	</nav>
+	<div class="alert alert-success text-center">
+		Data Mahasiswa
+	</div>
+	<div class="container">
+		<table class="table table-bordered table-sm">
+			<tr>
+				<th>No</th>
+				<th>Nim</th>
+				<th>Nama</th>
+				<th>Alamat</th>
+				<th>Aksi</th>
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>1810330067</td>
+				<td>Chambera</td>
+				<td>Sumbawa</td>
+				<td>
+					<a href="" class="btn btn-info btn-sm">Edit</a>
+					<a href="" class="btn btn-danger btn-sm">Hapus</a>
+				</td>
+			</tr>
+			
+		</table>
+		
+	</div>
+		
 
 </body>
 </html>

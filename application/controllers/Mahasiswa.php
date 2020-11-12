8d96b7da1e1252fb84c7c5fe67627f1bf3d40557
ArdianChambera <ardiancambera@gmail.com>
2020-11-12 17:31:03 +0800

    update v_index, mahasiswa_index, koneksi database

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -3,19 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Mahasiswa extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('M_mahasiswa');
+	}
+
 	
 	public function index()
 	{
+		$data['tabel'] = $this->M_mahasiswa->semuaData();
+		//variable dummie untuk cek data dari $data
+		//var_dump($data);
+		//die();
+
 		//load dari folder view
-		$data ['nama_lengkap'] = "Chambera"
-		$data ['jenis_kelamin'] = "Laki"
-		$mahasiswa[0] = array(
-			'nim' => 1810330067
-			'nama' => 'Chambera'
-			'profil' => 'Data Diri'
-		);
-		$data['mahasiswa'] = $mahasiswa;
-		$this->load->view('mahasiswa/v_index',$data);
+		$this->load->view('mahasiswa/v_index', $data);
 	}
 	
 	public function create()
--- application/views/mahasiswa_index.php
+++ application/views/mahasiswa_index.php
@@ -1,49 +1,9 @@
-<!DOCTYPE html>	
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<title>Mahasiswa Index</title>
-	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
+    <title>index</title>
 </head>
 <body>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<a href="" class="navbar-brand">
-			Tugas
-		</a>
-		<div class="navbar-nav">
-			<a href="" class="nav-link active">Home</a>
-			<a href="" class="nav-link">Tambah Data</a>
-		</div>
-		<div class="navbar-nav ml-auto">
-			<a href="" class="nav-link active">cambe</a>
-		</div>
-	</nav>
-	<div class="alert alert-success text-center">
-		Data Mahasiswa
-	</div>
-	<div class="container">
-		<table class="table table-bordered table-sm">
-			<tr>
-				<th>No</th>
-				<th>Nim</th>
-				<th>Nama</th>
-				<th>Alamat</th>
-				<th>Aksi</th>
-			</tr>
-			<tr>
-				<td>1</td>
-				<td>1810330067</td>
-				<td>Chambera</td>
-				<td>Sumbawa</td>
-				<td>
-					<a href="" class="btn btn-info btn-sm">Edit</a>
-					<a href="" class="btn btn-danger btn-sm">Hapus</a>
-				</td>
-			</tr>
-			
-		</table>
-		
-	</div>
-		
-
+    
 </body>
 </html>
